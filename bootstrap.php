4b16bc27076626ec6a6f020e4b257a913ffbde44
Weijie Fu <weijie.fu@studio.unibo.it>
2025-03-18 13:28:16 +0100

    bootstrap implemented

---
+++ bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+require_once("db/database.php");
+$dbh = new DatabaseHelper("localhost", "root", "", "spoti5", 3306); // Change port in case of issues with XAMPP.
+define("UPLOAD_DIR", "./upload/");
+?>
