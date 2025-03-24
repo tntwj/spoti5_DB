acee40e276550cbcd039344218214e04f65610c2
Weijie Fu <weijie.fu@studio.unibo.it>
2025-03-24 14:54:49 +0100

    add playlist and user login check functionality

---
+++ playlist.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'bootstrap.php';
+$templateParams["title"] = "Spoti5 - Home";
+$templateParams["main-content"] = "template/landing-page.php";
+if (isUserLoggedIn()) {
+    ;
+}
+require_once('template/base.php');
+?>
--- utils/functions.php
+++ utils/functions.php
@@ -5,4 +5,9 @@ class SessionKey {
     public const EMAIL = "email";
 }
 
+function isUserLoggedIn() {
+    return session_status() === PHP_SESSION_ACTIVE && ($_SESSION[SessionKey::LOGGED_IN] ?? null) === true;
+}
+
+
 ?>
