fb7e76cf37586275d8d6d01acd57c87554d6f9c8
Weijie Fu <weijie.fu@studio.unibo.it>
2025-03-29 15:57:53 +0100

    add song selection to create playlist form and streamline song retrieval

--- create-playlist.php
+++ create-playlist.php
@@ -4,6 +4,7 @@ $templateParams["title"] = "Spoti5 - Home";
 $templateParams["main-content"] = "template/creating-playlist-form.php";
 if (isUserLoggedIn()) {
     $templateParams["ID_user"] = $_SESSION[SessionKey::EMAIL];
+    $templateParams["songs"] = $dbh->getSongs();
 }                                                                      
 require_once('template/base.php');
 ?>
--- songs.php
+++ songs.php
@@ -2,6 +2,6 @@
 require_once 'bootstrap.php';
 $templateParams["title"] = "Spoti5 - Home";
 $templateParams["main-content"] = "template/view-songs.php";
-$templateParams["songs"] = $dbh->getSongs();;
+$templateParams["songs"] = $dbh->getSongs();
 require_once('template/base.php');
 ?>
