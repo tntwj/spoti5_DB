c4ed4ecbf831fb3de24914e4693adc443ec0d885
Weijie Fu <weijie.fu@studio.unibo.it>
2025-03-30 21:04:24 +0200

    style: update CSS for improved layout and color scheme; modify button text in category templates

--- template/category-options.php
+++ template/category-options.php
@@ -1,5 +1,6 @@
 <?php foreach($templateParams["options"] as $option): ?>
     <div class="option">
         <h2><?php echo htmlspecialchars($option["opzione"]); ?></h2>
+        <a href="songs-classified.php?category=<?php echo urlencode($option['opzione']); ?>">View Songs</a>
     </div>
 <?php endforeach; ?>
