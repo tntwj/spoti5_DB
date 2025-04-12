7f8de50f4781aa91550fc33833bac8c19b947efb
Weijie Fu <weijie.fu@studio.unibo.it>
2025-04-12 13:24:48 +0200

    debug: user list retrieval by removing login check

--- user-list.php
+++ user-list.php
@@ -2,7 +2,5 @@
 require_once 'bootstrap.php';
 $templateParams["title"] = "Spoti5 - User List";
 $templateParams["main-content"] = "template/user-list.php";
-if (isUserLoggedIn()) {
-    $templateParams["users"] = $dbh->getAllUsers();
-}                                                                      
+$templateParams["users"] = $dbh->getAllUsers();                                                                  
 require_once('template/base.php');
