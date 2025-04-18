ac6e7ca84086b5ef7bcdc72fff696f0e801a9443
Weijie Fu <weijie.fu@studio.unibo.it>
2025-04-18 17:53:37 +0200

    refactor: remove unused remove album tab and associated scripts for cleaner album management

--- template/album-management.php
+++ template/album-management.php
@@ -11,9 +11,6 @@
         <li class="nav-item">
             <a class="nav-link active" id="add-tab" data-toggle="tab" href="#add" role="tab">Add Album</a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" id="remove-tab" data-toggle="tab" href="#remove" role="tab">Remove Album</a>
-        </li>
     </ul>
     
     <div class="tab-content mt-3" id="albumTabContent">
@@ -45,64 +42,5 @@
                 <button type="submit" class="btn btn-primary">Add Album</button>
             </form>
         </div>
-        
-        <!-- Remove Album Tab -->
-        <div class="tab-pane fade" id="remove" role="tabpanel">
-            <form method="POST" action="">
-                <input type="hidden" name="action" value="remove">
-                
-                <div class="form-group">
-                    <label for="id_artista_remove">Artist:</label>
-                    <select class="form-control" id="id_artista_remove" name="id_artista" required>
-                        <option value="">-- Select an artist --</option>
-                        <?php foreach($templateParams["artists"] as $artist): ?>
-                        <option value="<?php echo $artist["ID_artista"]; ?>"><?php echo $artist["nome_d_arte"]; ?></option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-                
-                <div class="form-group">
-                    <label for="titolo_remove">Album Title:</label>
-                    <select class="form-control" id="titolo_remove" name="titolo" required disabled>
-                        <option value="">-- Select Artist First --</option>
-                    </select>
-                </div>
-                
-                <div class="alert alert-warning">
-                    <strong>Warning!</strong> Removing an album will also remove all tracks associated with it. This action cannot be undone.
-                </div>
-                
-                <button type="submit" class="btn btn-danger">Remove Album</button>
-            </form>
-        </div>
     </div>
 </div>
-
-<script>
-$(document).ready(function() {
-    // When artist changes in remove tab, load albums for that artist
-    $("#id_artista_remove").change(function() {
-        const artistId = $(this).val();
-        if (artistId) {
-            $("#titolo_remove").prop("disabled", false);
-            
-            $.ajax({
-                url: "get-artist-albums.php",
-                method: "POST",
-                data: { id_artista: artistId },
-                dataType: "json",
-                success: function(data) {
-                    let options = '<option value="">-- Select an album --</option>';
-                    data.forEach(function(album) {
-                        options += `<option value="${album.titolo}">${album.titolo}</option>`;
-                    });
-                    $("#titolo_remove").html(options);
-                }
-            });
-        } else {
-            $("#titolo_remove").prop("disabled", true);
-            $("#titolo_remove").html('<option value="">-- Select Artist First --</option>');
-        }
-    });
-});
-</script>
